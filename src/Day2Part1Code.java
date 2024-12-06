a1cc29359df4dc1f53a5184dd3e1a9b67bd99977
BT_4N2_15 <sample@bths.edu>
2024-12-06 14:05:46 -0500

    new

--- src/Day2Part1Code.java
+++ src/Day2Part1Code.java
@@ -5,17 +5,37 @@ import java.util.Scanner;
 
 public class Day2Part1Code {
     public static void main(String[] args) {
-
+        int count=0;
         ArrayList<String> fileData = getFileData("src/Day2Input");
         System.out.println(fileData);
         for(int i=0;i<fileData.size();i++){
             String[] split=fileData.get(i).split(" ");
             ArrayList<Integer> level=new ArrayList<Integer>();
+            boolean increasing;
+            boolean safe=true;
             for(int j=0;j<split.length;j++){
-                level.add(Integer.parseInt(split[i]));
+                level.add(Integer.parseInt(split[j]));
             }
-
+            if(level.get(0)<level.get(1)){
+                increasing=true;
+            } else {
+                increasing=false;
+            }
+            for(int j=1;j<level.size()-1;j++){
+                if(!increasing && level.get(j)<level.get(j+1) || increasing && level.get(j)>level.get(j+1)){
+                    safe=false;
+                }
+            }
+            for(int j=0;j<level.size()-1;j++){
+                if(Math.abs(level.get(j)-level.get(j+1)) < 1 || Math.abs(level.get(j)-level.get(j+1)) > 3){
+                    safe=false;
+                }
+            }
+            if(safe){
+                count++;
+            }
         }
+        System.out.println(count);
 
     }
 
--- src/Day2Part2Code.java
+++ src/Day2Part2Code.java
@@ -1,2 +1,88 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Day2Part2Code {
+    public static void main(String[] args) {
+        int count=0;
+        ArrayList<String> fileData = getFileData("src/Day2Input");
+        System.out.println(fileData);
+        for(int i=0;i<fileData.size();i++){
+            String[] split=fileData.get(i).split(" ");
+            ArrayList<Integer> level=new ArrayList<Integer>();
+            boolean increasing;
+            boolean safe=true;
+            boolean dampened=false;
+            for(int j=0;j<split.length;j++){
+                level.add(Integer.parseInt(split[j]));
+            }
+            if(level.get(0)<level.get(1)){
+                increasing=true;
+            } else {
+                increasing=false;
+            }
+            int levelSize=level.size();
+            for(int j=1;j<levelSize-1;j++){
+                if(!increasing && level.get(j)<level.get(j+1) || increasing && level.get(j)>level.get(j+1)){
+                    if(!dampened){
+                        level.remove(j);
+                        if(j>0){j-=1;}
+                        levelSize-=1;
+                        dampened=true;
+                        if(!increasing && level.get(j)<level.get(j+1) || increasing && level.get(j)>level.get(j+1)) {
+                            safe=false;
+                        }
+
+                        }else{
+                        safe=false;
+                    }
+
+                }
+            }
+            for(int j=0;j<levelSize-1;j++){
+                if(Math.abs(level.get(j)-level.get(j+1)) < 1 || Math.abs(level.get(j)-level.get(j+1)) > 3){
+                    if(!dampened){
+                        level.remove(j);
+                        if(j>0){j-=1;}
+                        levelSize-=1;
+                        dampened=true;
+                        if(Math.abs(level.get(j)-level.get(j+1)) < 1 || Math.abs(level.get(j)-level.get(j+1)) > 3){
+                            safe=false;
+                        }
+
+                    }else{
+                        safe=false;
+                    }
+
+                }
+            }
+            for(int j=0;j<levelSize;j++){
+                System.out.print(level.get(j) + " ");
+            }
+            System.out.println();
+            if(safe){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
 }
